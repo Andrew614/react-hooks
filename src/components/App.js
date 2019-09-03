6f2411e4f0063d6d23d4a4d84e7d1a198201d4df
Andrew614 <azhang213@gmail.com>
2019-09-03 19:13:58 -0400

    add a voting system

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,16 @@ import Button from './Button'
 
 const App = ({ anecdotes }) => {
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0))
+  console.log(points)
 
   return (
     <div>
       {anecdotes[selected]}
-      <Button randomAnecdotes={() => setSelected(Math.floor(Math.random() * anecdotes.length))} />
+      <p>has {points[selected]} votes</p>
+      <Button randomAnecdotes={() => setSelected(Math.floor(Math.random() * anecdotes.length))}
+        onPoints={() => setPoints(points.map((point, index) => index === selected ? points[index] = point + 1 : point))}
+      />
     </div>
   )
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const Button = ({ randomAnecdotes }) => {
+const Button = ({ randomAnecdotes, onPoints }) => {
     return (
         <div>
+            <button onClick={onPoints}>vote</button>
             <button onClick={randomAnecdotes}>next anecdote</button>
         </div>
     )
